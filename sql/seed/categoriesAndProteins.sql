923e6a2b7dc9165628267ae0efe08d8d9f640686
Sarah dePalo <sarahdepalo99@gmail.com>
2021-08-30 16:48:50 -0400

    finished seed for entrees and created schema

---
+++ sql/seed/categoriesAndProteins.sql
@@ -0,0 +1,52 @@
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Italian');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Mexican');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Thai');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Japanese');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Mediterranean');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Indian');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Cajun');
+INSERT INTO categories 
+    (category_name)
+VALUES 
+    ('Chinese');
+
+INSERT INTO proteins 
+    (protein_name)
+VALUES
+    ('Red Meat');
+
+INSERT INTO proteins 
+    (protein_name)
+VALUES
+    ('Poultry');
+
+INSERT INTO proteins 
+    (protein_name)
+VALUES
+    ('Seafood');
+
+INSERT INTO proteins 
+    (protein_name)
+VALUES
+    ('Vegetarian');
---
+++ sql/seed/entrees.sql
@@ -0,0 +1,443 @@
+-- ITALIAN
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Lasagna', 1, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Spaghetti', 1, 1);
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Ragu Alla Bolognese', 1, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Pizza', 1, 1, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Roasted Gnocchi', 1, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Shrimp Scampi', 1, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Shrimp Pasta Alla Vodka', 1, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Chicken Parmesan', 1, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Tuscan Chicken', 1, 2);
+
+-- MEXICAN
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Chicken Quesadillas', 2, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Chilaquiles', 2, 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Gorditas', 2, 4, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Fish Tacos', 2, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Mexican Beef Stew', 2, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Tamales', 2, 1, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Huevos Rancheros', 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Shredded Beef Tacos', 2, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Red Chile and Pork Stew', 2, 1);
+    
+-- THAI
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Thai Chicken Curry', 3, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Pad Thai', 3, 2, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Drunken Noodles', 3, 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Peanut Chicken and Noodles', 3, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Thai Red Chicken Curry', 3, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Khao Pad (Fried Rice)', 3, 4, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Tom Yum Goong (Spicy Shrimp Soup)', 3, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Thai Coconut Soup', 3, 3, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Thai Cashew Chicken Stir Fry', 3, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Thai Basil Beef', 3, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Spicy Beef Pad Thai', 3, 1);
+
+-- JAPANESE
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Sushi', 4, 3, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Teriyaki Salmon', 4, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Yaki Udon', 4, 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Soba Noodle Salad', 4, 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Gyudon (Beef Bowl)', 4, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Baked Tonkatsu', 4, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Ginger Pork', 4, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Yakisoba', 4, 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Vegan Miso Soup', 4, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Yakitori', 4, 2);
+
+-- Mediterranean 
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Shrimp Orzo Salad', 5, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Gyros', 5, 1, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Slow-Cooked Moroccan Chicken', 5, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Chicken Orzo Soup', 5, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Moroccan Pot Roast', 5, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Chickpea Stew', 5, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Greek Salad ', 5, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Spanakopita (Spinach and Cheese Pie)', 5, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Baked Lemon Garlic Salmon', 5, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Baked Cod with Lemon and Garlic', 5, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Lemony Greek Chicken Soup', 5, 2);
+
+-- INDIAN
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Chicken Biryani', 6, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Butter Chicken', 6, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Tandoori Chicken', 6, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Chickpea Curry', 6, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Beef Kabob', 6, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Mushroom Biryani', 6, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Tikka Masala', 6, 2, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Beef Curry', 6, 1);
+
+ INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Rogan Josh (Lamb Curry)', 6, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Fish Biryani', 6, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Seafood Curry', 6, 3);
+
+-- CAJUN
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Creole Jambalaya', 7, 3, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Gumbo', 7, 3, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Seared Scallops in Cajun Mustard Sauce', 7, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Blackened Catfish', 7, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Cajun Chicken Rice Bake', 7, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Chicken and Dirty Rice', 7, 2, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Shrimp and Grits', 7, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Okra Creole Over Rice', 7, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Po Boys', 7, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Red Beans and Rice', 7, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Vegetarian Gumbo', 7, 4);
+
+-- CHINESE
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Lo Mein', 8, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Sweet and Sour Chicken', 8, 2);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Beef and Broccoli', 8, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Hunan Kung Pao', 8, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Sesame Chicken', 8, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein, protein2)
+VALUES 
+    ('Fried Rice', 8, 2, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('General Tao Chicken', 8, 3);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Orange Pork Chop Stir-Fry', 8, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Vegetable Chow Mein', 8, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES
+    ('Chinese Beef Chow Fun', 8, 1);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Egg Drop Soup', 8, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Red Braised Pork Belly', 8, 4);
+
+INSERT INTO entrees 
+    (entree_name, category, protein)
+VALUES 
+    ('Kung Pao Shrimp', 8, 3);
+
+
+
+
+
+
+
+
